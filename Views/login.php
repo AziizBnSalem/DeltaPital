04f96c8556031eb0343cca95070880be0a9bdb67
AziizBnSalem <mohamedaziz.bensalem@gmail.com>
2025-05-07 04:27:44 +0100

    Remove name input field from login form to streamline user authentication process.

--- Views/login.php
+++ Views/login.php
@@ -127,7 +127,6 @@ include '../Controlleur/LoginController.php';
             ?>
 
             <form method="POST">
-                <input type="text" name="name" placeholder="Name" required>
                 <input type="email" name="email" placeholder="Email" required>
                 <input type="password" name="password" placeholder="Password" required>
                 <button type="submit">Login</button>
